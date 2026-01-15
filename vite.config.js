4efe0329a564b8d7a176600db5a2c00080c2a38a
playful-sound343 <ruturajajoshi2006@gmail.com>
2026-01-15 23:05:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
-export default {
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
     }
   }
-}
+})
+
